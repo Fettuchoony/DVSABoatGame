71316980d2f7421e44bd52eb073dd3acf5d16303
WilliamPickle <william1lopezperez@gmail.com>
2025-02-08 11:18:13 -0800

    Boat movement
    Added player movement script and exit marker for map[.

---
+++ Assets/Scripts/BoatMovement.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Numerics;
+using Unity.Mathematics;
+using Unity.VisualScripting.ReorderableList;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+ private Keyboard curKeyboard = Keyboard.current;
+ private Rigidbody rb;
+ private float movementX;
+ private float movementY;
+ private float rotationY = 0f;
+ public float speed = 0; 
+ public float maxSpeed = 10;
+ public float rotationSpeed = 0;
+
+private bool deboucne = false;
+ public float drag = 0f;
+ public Transform front;
+ private int isGoingForward;
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update(){
+        if(curKeyboard.wKey.isPressed){
+            if(math.abs(rb.linearVelocity.x)+math.abs(rb.linearVelocity.z) <= maxSpeed){
+                rb.AddForce(1 * transform.forward * speed);
+            }
+        }else if(curKeyboard.sKey.isPressed){
+            rb.AddForce(0.5f*transform.forward*-speed/2);
+        }
+        if(curKeyboard.aKey.isPressed){
+            transform.eulerAngles += UnityEngine.Vector3.down;
+        }
+        if(curKeyboard.dKey.isPressed){
+            transform.eulerAngles += UnityEngine.Vector3.up;
+        }
+    }
+    private void FixedUpdate(){
+        rb.linearVelocity =  drag*rb.linearVelocity;
+    }
+}
