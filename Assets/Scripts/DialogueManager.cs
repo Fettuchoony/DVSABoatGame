dbd36f04b0ae7af1395e9735458e281cc604c8ca
WilliamPickle <william1lopezperez@gmail.com>
2025-05-21 16:45:02 -0700

    Text stuff
    Sample of the text stuff

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,33 +1,67 @@
 using UnityEngine;
 using System.Collections.Generic;
 using TMPro;
+using System.Collections;
+using Unity.VisualScripting;
 
 public class DialogueManager : MonoBehaviour
 {
-    
-    public TextMeshProUGUI boxText;
+    public GameObject textBox;
+    public GameObject dialogueObject;
+    public float dialogueFadeInSpeed;
+
+    private TextMeshProUGUI  dialogue;
     private int textID;
+    private string test = "WASDWASDWASDWASD";
 
     private Dictionary<int, string> currDialogue;
 
+    private RectTransform rectTrans;
+
+    private float gameTime;
+    private float waitTime;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         currDialogue = new Dictionary<int, string>();
-
-
+        dialogue = dialogueObject.GetComponent<TextMeshProUGUI>();
+        rectTrans = dialogueObject.GetComponent<RectTransform>();
+        gameTime = 0.5f;
+        StartCoroutine(animatedText("Edwin I wanna get more money PLEASE!!"));
+        waitTime = Time.time;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-   
+        // dialogueObject.transform.position = Vector3.Lerp(new Vector3(0,0,0),new Vector3(0,10,0),1f);
+
+        if(textBox.activeSelf && Time.time - waitTime > 5f){
+            rectTrans.anchoredPosition = Vector3.Lerp(new Vector3(0,0,0),new Vector3(0,50,0),  gameTime);
+            gameTime *= 1f + dialogueFadeInSpeed * Time.deltaTime;
+        }
     }
 
-    public void toggleDialogue(bool enabled) {
-        boxText.SetActive(enabled);
+    // void FixedUpdate()
+    // {
+        
+    // }
+
+    IEnumerator animatedText(string text){
+        foreach(char c in text.ToCharArray()){
+            dialogue.text += c;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+    public void toggleDialogue(bool enabled,int textID) {
+        dialogue.SetText("");
+        textBox.SetActive(enabled);
+        if(enabled){
+            waitTime = Time.deltaTime;
+            StartCoroutine(animatedText(currDialogue[textID]));
+        }
     }
 
     public void changeDialogueID(int ID) {
