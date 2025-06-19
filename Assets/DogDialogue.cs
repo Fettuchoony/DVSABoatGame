c6645e89b7063637f3f127d5786b2f0edfaa2fd9
Fettuchoony <106009124+Fettuchoony@users.noreply.github.com>
2025-06-18 17:01:26 -0700

    Fixed UI Scaling

--- Assets/DogDialogue.cs
+++ Assets/DogDialogue.cs
@@ -12,6 +12,7 @@ public class DogDialogue : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        currLevel = 1;
         GameObject dialogueObj = GameObject.Find("Dialogue");
         if (dialogueObj == null) {
             Debug.Log("Could not find dialogue object from dog dialogue");
@@ -39,40 +40,49 @@ public class DogDialogue : MonoBehaviour
         compLvls = inv.completedLevels;
         // lvl 1
         if (currLevel == 1 && !compLvls[currLevel-1]) {
+            currLevel++;
             // Nothing
         }
         // lvl 2
         if (currLevel == 2 && !compLvls[currLevel-1]) {
             Debug.Log("Dog triggered");
             dialogue.toggleDialogue(true, 100 + 10*currLevel);
+            currLevel++;
         }
         // lvl 3
         if (currLevel == 3 && !compLvls[currLevel-1]) {
             dialogue.toggleDialogue(true, 100 + 10*currLevel);
+            currLevel++;
         }
         // lvl 4
         if (currLevel == 4 && !compLvls[currLevel-1]) {
             dialogue.toggleDialogue(true, 100 + 10*currLevel);
+            currLevel++;
         }
         // lvl 5
         if (currLevel == 5 && !compLvls[currLevel-1]) {
             dialogue.toggleDialogue(true, 100 + 10*currLevel);
+            currLevel++;
         }
         // lvl 6
         if (currLevel == 6 && !compLvls[currLevel-1]) {
             dialogue.toggleDialogue(true, 100 + 10*currLevel);
+            currLevel++;
         }
         // lvl 7
         if (currLevel == 7 && !compLvls[currLevel-1]) {
             dialogue.toggleDialogue(true, 100 + 10*currLevel);
+            currLevel++;
         }
         // lvl 8
         if (currLevel == 8 && !compLvls[currLevel-1]) {
             dialogue.toggleDialogue(true, 100 + 10*currLevel);
+            currLevel++;
         }
         // lvl 9
         if (currLevel == 9 && !compLvls[currLevel-1]) {
             dialogue.toggleDialogue(true, 100 + 10*currLevel);
+            currLevel++;
         }
     }
 }
